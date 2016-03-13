31e7d851fbcd093bac281398a5e2b0b144d7beea
Josh <joshuaskling@gmail.com>
2016-03-12 17:12:26 -0800

    main program now catches tweets in a stream and tweets a cat pic back at the user

--- live_laser_cat_main.py
+++ live_laser_cat_main.py
@@ -1,9 +1,10 @@
 import tweepy
+import time
 import servo_driver, camera_driver
 
 #set OAuth information
-consumer_key = "Bb8BlQ8hFqaH6b5gqu4LPyRR1"
-consumer_secret = "VmtljceTYOPLr3hvkvmhu7FQzLeLRxrrOK4LPv8GF5RFXADGyY"
+consumer_key = "qNfUHr4XUyoaeTapyMyk7quYG"
+consumer_secret = "0gRguuOwK5t3YuyhAJIxny5JMoaV8Hon6mioNUnGdCTEjpfe2W"
 access_token = "708766117678817280-ehanQvqL8LUnx15uvUXjJDynotWbaSz"
 access_token_secret = "uCbdHeJ6jZkX2Bg7EAteKofhjYAnO5MP7h8xbMR6kuWVi"
 
@@ -14,26 +15,53 @@ auth.set_access_token(access_token, access_token_secret)
 #create api access point
 api = tweepy.API(auth)
 
-#post test pic
-def postTestPic():
-    api.update_with_media("content\\cat_test.jpg", "test status")
+#post cat photo (currently in test mode)
+def postNewPhoto(user):
+    input = user.decode('utf-8')
+    api.update_with_media("content\\cat_test.jpg", "Cat photo taken for @" + input)
 
 #stream class
 class MyStreamListener(tweepy.StreamListener):
     
     #print stream
     def on_status(self, status):
-        print(status.text.encode('utf-8'))
+        print(status.author.screen_name.encode('utf-8') + status.source.encode('utf-8') + status.text.encode('utf-8'))
+        postNewPhoto(status.author.screen_name.encode('utf-8'))
     
     #handle error codes
     def on_error(self, status_code):
         if (status_code == 420):
+            print ("test")
             return False
-
+            
 #create stream
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
 
-#myStream.filter(follow="@LiveLaserCat", track=['test'], async=True)
+#run stream
+myStream.filter(track=['@livelasercat'], async=True)
 
-#postTestPic()
+"""    
+#main loop method checks for messages every x minutes
+while(1):
+    #set timer
+    if (sent == True):
+        timer = time.clock()
+        sent = False
+    
+    #get direct messages at interval
+    if (time.clock() - timer > 10):
+        messages = api.direct_messages()
+        for message in messages:
+            print (message)
+        
+        #check for new messages
+        #if new message, send data
+        postNewPhoto("@testuser")
+"""
+"""
+#print all tweets
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print (tweet.text)
+"""
