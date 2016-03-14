959f35005f96af7f2421ab1e2d9b04494542b581
Josh <joshuaskling@gmail.com>
2016-03-14 14:21:53 -0700

    added reddit usage

--- live_laser_cat_main.py
+++ live_laser_cat_main.py
@@ -1,5 +1,7 @@
-import tweepy
-import time
+import tweepy, praw, requests
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import servo_driver, camera_driver
 
 #set OAuth information
@@ -14,6 +16,57 @@ auth.set_access_token(access_token, access_token_secret)
 
 #create api access point
 api = tweepy.API(auth)
+
+#get top posts from showerthoughts and aww
+def getRedditContent():
+    print("Getting Reddit content")
+    r = praw.Reddit(user_agent="livelasercat")
+
+    #get submissions
+    print("Getting image...")
+    submissions = r.get_random_submission('aww')
+
+    url = submissions.url
+    print (url)
+    #for x in submissions:
+    #    url = x.url
+
+    #download image to local machine
+    image = open("temp.jpg", "wb")
+    image.write(requests.get(url).content)
+    image.close()
+    print("Image downloaded")
+
+    #get text
+    text = ""
+    print("Getting showerthought...")
+    submissions = r.get_random_submission('showerthoughts')
+
+    #for item in submissions:
+    #    text = item
+    text = str(submissions).split(":: ")
+    text = text[1]
+    print (text)
+    #print ("Showerthought downloaded")
+
+    #apply text to picture
+    print ("Applying text to image...")
+
+    #define fonts
+    font = ImageFont.truetype("C:\Windows\\Fonts\\Calibri.ttf",40)
+    textColor = (255, 255, 255)
+    textPosition = (100,100)
+
+    #open image and draw
+    image = Image.open("temp.jpg")
+    draw = ImageDraw.Draw(image)
+    draw.text(textPosition, text, fill=textColor, font=font)
+    del draw
+
+    #save image
+    image.save("temp.jpg")
+    image.close()
+
 
 #post cat photo (currently in test mode)
 def postNewPhoto(user):
@@ -34,6 +87,8 @@ class MyStreamListener(tweepy.StreamListener):
             print ("test")
             return False
             
+getRedditContent()
+
 #create stream
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
